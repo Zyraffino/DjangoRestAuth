75ea354bb0360fb881964ab215bee559607c9aed
ZYRAFFINO <zyraffino@gmail.com>
2019-11-23 14:40:11 +0900

    staticfile fixed, frontend inserted

--- Turttle/settings.py
+++ Turttle/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/templates/'
-# STATICFILES_DIRS = [
-#     SCRIPTMANAGER_TEMPLATE_DIR
-# ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
